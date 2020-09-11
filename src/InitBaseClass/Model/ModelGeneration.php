e43834e8403915bbb820b31a783fa174410444df
tioncico <1067197739@qq.com>
2020-09-11 14:27:34 +0800

    修改依赖,修改baseModel

--- src/InitBaseClass/Model/ModelGeneration.php
+++ src/InitBaseClass/Model/ModelGeneration.php
@@ -47,5 +47,29 @@ try {
 }
 BODY
         );
+        $method = $this->phpClass->addMethod('getPageList');
+        $method->addParameter('page')->setType('int')->setDefaultValue(1);
+        $method->addParameter('pageSize')->setType('int')->setDefaultValue(20);
+        $method->addParameter('isCount')->setType('bool')->setDefaultValue(true);
+        $method->setBody(<<<BODY
+if (\$isCount){
+    \$this->withTotalCount();
+}
+\$list = \$this
+    ->page(\$page, \$pageSize)
+    ->all();
+\$data = [];
+\$data['list'] = \$list;
+\$data['page'] = \$page;
+\$data['pageSize']  = \$pageSize;
+if (\$isCount){
+    \$total = \$this->lastQueryResult()->getTotalCount();
+    \$data['total'] = \$total;
+    \$data['pageCount'] = ceil(\$total / \$pageSize);
+}
+
+return \$data;
+BODY
+        );
     }
 }
